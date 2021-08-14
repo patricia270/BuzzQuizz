1d18b1f2de4f19cf6ecc4ce84614d9aec04c01fc
patriciasilva.eq@gmail.com <patriciasilva.eq@gmail.com>
2021-08-14 14:17:51 -0400

    Atualizando...

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let quizzes = [];
 
 function getQuizzes() {
@@ -6,14 +5,37 @@ function getQuizzes() {
     getQuizzesPromise.then(listQuizzes);
 }
 
+function renderQuizzes() {
+    const ulQuizzes = document.querySelector(".all-quizzes .quizzes-list");
+    ulQuizzes.innerHTML = `
+        <li class="option" id="${quizzes[0].id}">
+            <h2>Todos os Quizzes</h2> 
+            <img class="image-quiz" src="${quizzes[0].image}">
+            <div class="image-quiz-box"></div>
+            <h3>${quizzes[0].title}</h3>
+        </li>
+        `;
+
+    for (let i = 1; i < quizzes.length; i++) {
+        ulQuizzes.innerHTML += `
+        <li class="option" id="${quizzes[i].id}">
+            <img class="image-quiz" src="${quizzes[i].image}">
+            <div class="image-quiz-box"></div>
+            <h3>${quizzes[i].title}</h3>
+        </li>
+        `;
+    }
+}
+
 function listQuizzes(listQuizzesResponse) {
     quizzes = listQuizzesResponse.data;
     console.log(quizzes);
+    renderQuizzes();
 }
 getQuizzes()
 
-function renderQuizzes() {
 
-}
+
+
 
 
